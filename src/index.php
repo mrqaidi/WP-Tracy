b68131ae3d4d6d3354fcefc46a28e3ec36a18a60
mrqaidi <mrqaidi@gmail.com>
2019-02-24 23:30:08 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -20,41 +20,19 @@ function wp_tracy_init_action()
         "WpTracy\\WpCurrentScreenPanel",
     ]; // in the correct order
 
-    $defaultSettings = [
-        "check-is-user-logged-in" => defined("WP_TRACY_CHECK_IS_USER_LOGGED_IN") ? WP_TRACY_CHECK_IS_USER_LOGGED_IN : "off",
-        "only-for-user-id" => defined("WP_TRACY_ONLY_FOR_USER_ID") ? WP_TRACY_ONLY_FOR_USER_ID : null,
-        "debugger-mode" => defined("WP_TRACY_ENABLE_MODE") ? WP_TRACY_ENABLE_MODE : "detect",
+    $settings = [
+        "only-for-user-id" =>  null,
         "panels-classes" => $defaultPanelsClasses,
-        "panels-filtering-allowed" => defined("WP_TRACY_PANELS_FILTERING_ALLOWED") ? WP_TRACY_PANELS_FILTERING_ALLOWED : "on",
+        "panels-filtering-allowed" =>  "on",
     ];
-
-    $userSettings = get_option("wp-tracy-user-settings", []);
-
-    $settings = wp_parse_args($userSettings, $defaultSettings);
-
-    if ($settings["check-is-user-logged-in"] === "on") {
-        $isUserLoggedIn = is_user_logged_in();
-        if (!$isUserLoggedIn) {
-            return; // cancel for anonymous users
-        }
-        $onlyForUserId = $settings["only-for-user-id"];
-        if ($onlyForUserId > 0 && $onlyForUserId != get_current_user_id()) {
-            return; // cancel other users
-        }
-    }
-
-    switch ($settings["debugger-mode"]) {
-        case "development":
-            $debugMode = Tracy\Debugger::DEVELOPMENT;
-            break;
-        case "production":
-            $debugMode = Tracy\Debugger::PRODUCTION;
-            break;
-        default:
-            $debugMode = Tracy\Debugger::DETECT;
-            break;
-    }
-    Tracy\Debugger::enable($debugMode); // hooray, enabling debugging using Tracy
+	$settings = apply_filters( 'mrqaidi_debugger_settings', $settings );
+	$debugMode = Tracy\Debugger::PRODUCTION;
+	$onlyForUserId = $settings["only-for-user-id"];
+	if ($onlyForUserId > 0 && $onlyForUserId = get_current_user_id()) {
+		$debugMode = Tracy\Debugger::DEVELOPMENT;
+	}
+	Tracy\Debugger::$showBar = true;
+    Tracy\Debugger::enable($debugMode,$settings["log-path"]); // hooray, enabling debugging using Tracy
 
     $panelsClasses = $settings["panels-classes"];
     if (!is_array($panelsClasses)) {
